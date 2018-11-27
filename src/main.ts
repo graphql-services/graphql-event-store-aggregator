31888b5f9ac14e7daf66deac4255b65490ff13c9
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-11-27 09:12:16 +0100

    Update typeorm to 0.2.9

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ENV } from 'env';
+import { ENV } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(ENV.PORT || 80);
 }
 bootstrap();
+
+interface AsyncIterator<T> {
+  next(value?: any): Promise<IteratorResult<T>>;
+  return?(value?: any): Promise<IteratorResult<T>>;
+  throw?(e?: any): Promise<IteratorResult<T>>;
+}
